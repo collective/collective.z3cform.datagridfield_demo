0c561993e5c0976465c2a7fb91d91f1a373ba296
kevingill <kevingill@localhost>
2011-02-08 21:49:36 +0000

    Initial import.
    svn path=/collective.z3cform.datagridfield_demo/trunk/; revision=231787

---
+++ README.txt
@@ -0,0 +1,27 @@
+Introduction
+============
+
+This package provides the demo views for collective.z3cform.datagridfield. See that 
+package for more information.
+
+Installation
+------------
+
+Add collective.z3cforms.datagridfield_demo to your buildout eggs.::
+
+    eggs=\
+        ...
+        collective.z3cforms.datagridfield
+        collective.z3cforms.datagridfield_demo
+
+Examples of configurations are in the demo folder.
+Once you install this package, the demo views will be visible on your site. ::
+
+    * http://localhost:8080/Plone/@@demo-collective.z3cform.datagrid
+
+References
+----------
+ 
+    * http://pypi.python.org/pypi/collective.z3cform.datagridfield
+
+
---
+++ setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup, find_packages
+import os
+
+version = '0.5dev'
+
+setup(name='collective.z3cform.datagridfield_demo',
+      version=version,
+      description="Version of DataGridField for use with Dexterity / z3c.form - Demo views",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from
+      # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        ],
+      keywords='Plone, Dexterity, z3c.form',
+      author='Kevin Gill',
+      author_email='kevin@movieextras.ie',
+      url='http://svn.plone.org/svn/collective/',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['collective', 'collective.z3cform'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+          'plone.app.z3cform',
+          'collective.z3cform.datagridfield',
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      setup_requires=["PasteScript"],
+      paster_plugins = ["ZopeSkel"],
+      )
